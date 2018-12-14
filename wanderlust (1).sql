33f0e9e69d4f784ea5c163e7b6e522debd505738
Denise <dfranan@gmail.com>
2018-12-13 21:03:12 -0300

    para que nos salven las papas

---
+++ wanderlust (1).sql
@@ -0,0 +1,108 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 14, 2018 at 01:00 AM
+-- Server version: 10.1.36-MariaDB
+-- PHP Version: 7.2.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `wanderlust`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(20) NOT NULL,
+  `apellido` varchar(20) NOT NULL,
+  `usuario` varchar(20) NOT NULL,
+  `password` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`id`, `nombre`, `apellido`, `usuario`, `password`) VALUES
+(13, 'Denise', 'Franco', 'den', '81dc9bdb52d04dc20036dbd8313ed055'),
+(14, 'lucia', 'lala', 'lucia', '81dc9bdb52d04dc20036dbd8313ed055'),
+(15, 'tomas', 'coca', 'tomas', '81dc9bdb52d04dc20036dbd8313ed055'),
+(16, 'lucas', 'r', 'lucas', '81dc9bdb52d04dc20036dbd8313ed055'),
+(17, 'nicolas', 'l', 'nico2', '81dc9bdb52d04dc20036dbd8313ed055'),
+(18, 'gustavo', 'lala', 'gus', '81dc9bdb52d04dc20036dbd8313ed055'),
+(19, 'maria', 'maria', 'mar', '81dc9bdb52d04dc20036dbd8313ed055');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `id_post` int(100) NOT NULL,
+  `id_autor` int(100) NOT NULL,
+  `titulo` varchar(50) NOT NULL,
+  `lugar` varchar(50) NOT NULL,
+  `post` varchar(999) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`id_post`, `id_autor`, `titulo`, `lugar`, `post`) VALUES
+(1, 0, 'amigas', 'san marcos, cordoba', 'sfjksldjfkdsljfds\r\nfdsjklfdjskfds\r\nfdjskflsdj\r\n');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id_post`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `login`
+--
+ALTER TABLE `login`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id_post` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
